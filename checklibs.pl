3e34a17ce8fe7a52a58f91962d8daa4eae9404dd
Marc Liyanage <reg.gitcommit@entropy.ch>
2009-07-05 17:57:49 +0200

    first checkin of checklibs.pl. See http://www.entropy.ch/blog/Developer/2009/07/05/Updated-checklibs-pl-script-to-list-dynamic-library-dependencies.html

--- checklibs.pl
+++ checklibs.pl
@@ -1,7 +1,11 @@
 #!/usr/bin/perl
 #
 # Check the dynamic library dependencies of a
-# Mac OS X binary
+# Mac OS X binary.
+# 
+# See
+#     http://www.entropy.ch/blog/Developer/2009/07/05/Updated-checklibs-pl-script-to-list-dynamic-library-dependencies.html
+# for more information.
 #
 # Written by Marc Liyanage <http://www.entropy.ch>
 #
@@ -10,6 +14,7 @@
 use strict;
 use warnings;
 
+use Term::ANSIColor;
 
 my ($file) = @ARGV;
 die "Usage: $0 file\n" unless $file;
@@ -20,18 +25,29 @@ my $libs = {};
 check_libs(file => $file, libs => $libs);
 
 print
-	map {("\n$_:\n", map {"\t$_\n"} sort {lc($a) cmp lc($b)} @{$libs->{$_}})}
+	map {(
+		sprintf("\n%s:\n", colorize($_)),
+		map {"\t$_\n"}
+		map {colorize($_)}
+		sort {lc($a) cmp lc($b)}
+		@{$libs->{$_}}
+	)}
 	sort {lc($a) cmp lc($b)}
 	grep {@{$libs->{$_}}}
 	keys(%$libs);
 
+
 sub check_libs {
 	my (%args) = @_;
 	my $libs = $args{libs};
-	my @file_libs = grep {$_ ne $args{file}} grep {$_} map {/^\s+(\S+)/} qx(otool -L '$args{file}');
+	my @file_libs =
+		grep {$_ ne $args{file}}
+		grep {$_}
+		map {/^\s+(\S+)/}
+		qx(otool -L '$args{file}');
 	$libs->{$args{file}} = \@file_libs;
 	foreach my $lib (grep {!$libs->{$_}} @file_libs) {
-		unless (-f $lib) {
+		unless (-e $lib) {
 			$libs->{$lib} = ['(missing)'];
 			next;
 		}
@@ -40,3 +56,7 @@ sub check_libs {
 }
 
 
+sub colorize {
+	my ($path) = @_;
+	return $path !~ m!^(/usr/lib|/System)! ? colored($path, 'red') : $path;
+}
