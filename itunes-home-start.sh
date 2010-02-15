9f2dc62d80fec5abcb4d462070c845992b65b5da
Marc Liyanage <liyanage@primavera.local>
2010-02-14 18:15:02 -0800

    itunes home sharing script tweaks

--- itunes-home-start.sh
+++ itunes-home-start.sh
@@ -7,6 +7,8 @@
 # - http://lists.apple.com/archives/bonjour-dev/2007/Jul/msg00031.html
 #
 dns-sd -P "Marc’s Home iTunes" _daap._tcp . 13689 something.local 127.0.0.1 "Arbitrary text record" &
+#dns-sd -P "Marc’s Home iTunes" _daap._tcp . 13689 something.local minime-marc.liyanage.members.mac.com. 3689 "Arbitrary text record" &
 PID=$!
-ssh -o ServerAliveInterval=60 -o ServerAliveCountMax=99 -C -N -L *:13689:localhost:3689 -6 minime-marc.liyanage.members.mac.com.
+/opt/local/bin/socat TCP4-LISTEN:13689,reuseaddr,fork TCP6:minime-marc.liyanage.members.mac.com:3689
+#ssh -o ServerAliveInterval=60 -o ServerAliveCountMax=99 -C -N -L *:13689:localhost:3689 -6 minime-marc.liyanage.members.mac.com.
 kill $PID
