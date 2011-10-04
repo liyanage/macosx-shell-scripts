1d7a9ecbd9b1b4fb43732a0ce430810432d59dca
Marc Liyanage <reg.github@entropy.ch>
2011-10-03 22:52:47 -0700

    Output tweaks

--- svnstatus.py
+++ svnstatus.py
@@ -3,7 +3,7 @@
 # Cleaned up "svn status" output
 #
 
-import subprocess
+import subprocess, time, sys
 
 popen = subprocess.Popen(['svn', 'status'], stdout = subprocess.PIPE)
 output = popen.communicate()[0]
@@ -18,12 +18,13 @@ for line in output.splitlines():
 
 	if line.startswith('Performing status on external'):
 		current_external = line
+		print "\x1b[2K\r",
+		print current_external,
+		sys.stdout.flush()
+		time.sleep(0.01)
 		continue
 
-	if current_external:
-		print current_external
-		current_external = ''
-
+	print
 	print line
 
-	
+print "\x1b[2K",
