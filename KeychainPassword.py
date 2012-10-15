5cd0ac368c9cb6843371939cdc46bf78f4a26b65
Marc Liyanage <reg.github@entropy.ch>
2012-10-15 11:16:18 -0700

    KeychainPassword.py cleanup

--- KeychainPassword.py
+++ KeychainPassword.py
@@ -3,28 +3,41 @@ import subprocess, re
 
 class KeychainPassword:
 
-	@classmethod
-	def find_internet_password(cls, username, host = None):
-		cmd = ['security', 'find-internet-password', '-g', '-a', username]
-		if host:
-			cmd.extend(['-s', host])
-		
-		return cls.run_security_command(cmd)
-		
-	@classmethod
-	def find_generic_password(cls, username, label = None):
-		cmd = ['security', 'find-generic-password', '-g', '-a', username]
-		if label:
-			cmd.extend(['-l', label])
-		
-		return cls.run_security_command(cmd)
-		
-	@classmethod
-	def run_security_command(cls, cmd):
-		securityProcess = subprocess.Popen(cmd, stderr = subprocess.PIPE, stdout = subprocess.PIPE)
-		stdout, stderr = securityProcess.communicate()
-		result = re.findall('password: "(.*)"', stderr)
-		if not result:
-			return None
-		(password,) = result
-		return password
+    @classmethod
+    def find_internet_password(cls, username, host=None):
+        cmd = ['security', 'find-internet-password', '-g', '-a', username]
+        if host:
+            cmd.extend(['-s', host])
+        
+        username, password = cls.run_security_command(cmd)
+        return password
+        
+    @classmethod
+    def find_generic_password(cls, username, label=None):
+        username, password = cls.find_generic_username_and_password(username=username, label=label)
+        return password
+        
+    @classmethod
+    def find_generic_username_and_password(cls, username=None, label=None):
+        cmd = ['security', 'find-generic-password', '-g']
+        if label:
+            cmd.extend(['-l', label])
+        if username:
+            cmd.extend(['-a', username])
+        return cls.run_security_command(cmd)
+
+    @classmethod
+    def run_security_command(cls, cmd):
+        try:
+            security_output = subprocess.check_output(cmd, stderr=subprocess.STDOUT)
+        except:
+            return None
+        
+        result = re.findall('password: "(.*?)"$.*"acct"<blob>="(.*?)"$', security_output, re.DOTALL|re.MULTILINE)
+        if not result:
+            return None
+        (password, username), = result
+
+        return username, password
+
+        
