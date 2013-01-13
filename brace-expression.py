e758c2599a9ad91bc015860f581f66777155f8ab
Marc Liyanage <reg.github@entropy.ch>
2013-01-12 18:52:49 -0800

    Handle occurrence of directory plus its children, handle spaces in filenames.

--- brace-expression.py
+++ brace-expression.py
@@ -1,4 +1,27 @@
 #!/usr/bin/env python
+#
+# Converts a set of paths (on stdin, one path per line) into a shell brace expression.
+#
+# Example:
+#
+# security
+# security/audit_class
+# security/audit_control
+# security/audit_event
+# security/audit_user
+# security/audit_warn
+# services
+# shells
+# slpsa.conf
+# smb.conf.old
+# snmp
+# snmp/snmpd.conf
+# snmp/snmpd.conf.default
+#
+# becomes
+#
+# {smb.conf.old,shells,snmp,snmp/{snmpd.conf.default,snmpd.conf},services,security,security/{audit_control,audit_class,audit_user,audit_warn,audit_event},slpsa.conf}
+
 
 import re
 import sys
@@ -8,11 +31,13 @@ class Node(object):
 
     def __init__(self, name=None):
         self.name = name
+        self.include_self = False
         self.children = {}
     
     def add_path(self, path):
         if not path:
             return
+        path = path.replace(' ', r'\ ')
         items = path.split('/')
         self.add_path_components(items)
 
@@ -20,8 +45,12 @@ class Node(object):
         if not path_components:
             return
         head = path_components[0]
+        tail = path_components[1:]
         child = self.children.setdefault(head, Node(head))
-        child.add_path_components(path_components[1:])
+        if tail:
+            child.add_path_components(tail)
+        else:
+            child.include_self = True
     
     def shell_brace_expression(self):
         if not self.children:
@@ -32,7 +61,10 @@ class Node(object):
             children = '{' + children + '}'
         
         if self.name:
-            return '/'.join([self.name, children])
+            result = ['/'.join([self.name, children])]
+            if self.include_self and children:
+                result.insert(0, self.name)
+            return ','.join(result)
         else:
             return children
 
