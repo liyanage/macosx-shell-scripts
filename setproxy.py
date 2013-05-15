612cb8b80ee70819df76e27693db3a52a2252c42
Marc Liyanage <reg.github@entropy.ch>
2013-05-15 00:36:12 -0700

    Fix detection of active network interface.

--- setproxy.py
+++ setproxy.py
@@ -11,6 +11,7 @@
 import subprocess
 import re
 
+
 def proxy_state_for_interface(interface):
     state = subprocess.check_output('networksetup -getwebproxy'.split() + [interface]).splitlines()
     return dict([re.findall(r'([^:]+): (.*)', line)[0] for line in state])
@@ -23,19 +24,26 @@ def disable_proxy_for_interface(interface):
     for subcommand in ['-setwebproxystate', '-setsecurewebproxystate']:
         subprocess.check_output(['networksetup', subcommand, interface, 'Off'])
     
-def interface_list():
-    return subprocess.check_output('networksetup -listallnetworkservices'.split()).splitlines()[1:]
-
 def first_interface():
-    return interface_list()[0]
+    scutil_script = 'get State:/Network/Global/IPv4\nd.show\n'
+    popen = subprocess.Popen('/usr/sbin/scutil', stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+    (stdout, stderr) = popen.communicate(scutil_script)
+    interface, = re.findall(r'PrimaryInterface\s*:\s*(.+)', stdout)
 
+    order = subprocess.check_output('networksetup -listnetworkserviceorder'.split())
+    mapping = re.findall(r'\(\d+\)\s(.*)$\n\(.*Device: (.+)\)$', order, re.MULTILINE)
+    mapping = dict([(b, a) for (a, b) in mapping])
+    
+    service_name = mapping[interface]
+    return service_name
+    
 
 first_interface = first_interface()
 
 state = proxy_state_for_interface(first_interface)
 if state['Enabled'] == 'Yes':
-    print 'Disabling proxy...'
+    print 'Disabling proxy on {}...'.format(first_interface)
     disable_proxy_for_interface(first_interface)
 else:
-    print 'Enabling proxy...'
+    print 'Enabling proxy on {}...'.format(first_interface)
     enable_proxy_for_interface(first_interface)
