2a0a233f645aab623ef970165c969fa2bfc52605
Marc Liyanage <reg.github@entropy.ch>
2013-05-15 15:14:38 -0700

    Add port option and convert to class-based tool

--- mitmproxywrapper.py
+++ mitmproxywrapper.py
@@ -9,74 +9,79 @@ import subprocess
 import re
 import argparse
 
-def run_networksetup_command(*arguments):
-    return subprocess.check_output(['sudo', 'networksetup'] + list(arguments))
-
-def proxy_state_for_service(service):
-    state = run_networksetup_command('-getwebproxy', service).splitlines()
-    return dict([re.findall(r'([^:]+): (.*)', line)[0] for line in state])
-
-def enable_proxy_for_service(service):
-    print 'Enabling proxy on {}...'.format(service)
-    for subcommand in ['-setwebproxy', '-setsecurewebproxy']:
-        run_networksetup_command(subcommand, service, '127.0.0.1', '8080')
-
-def disable_proxy_for_service(service):
-    print 'Disabling proxy on {}...'.format(service)
-    for subcommand in ['-setwebproxystate', '-setsecurewebproxystate']:
-        run_networksetup_command(subcommand, service, 'Off')
-
-def interface_name_to_service_name_map():    
-    order = run_networksetup_command('-listnetworkserviceorder')
-    mapping = re.findall(r'\(\d+\)\s(.*)$\n\(.*Device: (.+)\)$', order, re.MULTILINE)
-    return dict([(b, a) for (a, b) in mapping])
-
-def run_command_with_input(command, input):
-    popen = subprocess.Popen(command, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
-    (stdout, stderr) = popen.communicate(input)
-    return stdout
+class Wrapper(object):
     
-def primary_interace_name():
-    scutil_script = 'get State:/Network/Global/IPv4\nd.show\n'
-    stdout = run_command_with_input('/usr/sbin/scutil', scutil_script)
-    interface, = re.findall(r'PrimaryInterface\s*:\s*(.+)', stdout)
-    return interface
+    def __init__(self, port):
+        self.port = port
+        self.primary_service_name = self.find_primary_service_name()
 
-def primary_service_name():
-    return interface_name_to_service_name_map()[primary_interace_name()]
+    def run_networksetup_command(self, *arguments):
+        return subprocess.check_output(['sudo', 'networksetup'] + list(arguments))
 
-def proxy_enabled_for_service(service):
-    return proxy_state_for_service(service)['Enabled'] == 'Yes'
+    def proxy_state_for_service(self, service):
+        state = self.run_networksetup_command('-getwebproxy', service).splitlines()
+        return dict([re.findall(r'([^:]+): (.*)', line)[0] for line in state])
 
-def toggle_proxy():
-    service_name = primary_service_name()
+    def enable_proxy_for_service(self, service):
+        print 'Enabling proxy on {}...'.format(service)
+        for subcommand in ['-setwebproxy', '-setsecurewebproxy']:
+            self.run_networksetup_command(subcommand, service, '127.0.0.1', str(self.port))
 
-    if proxy_enabled_for_service(service_name):
-        disable_proxy_for_service(service_name)
-    else:
-        enable_proxy_for_service(service_name)
+    def disable_proxy_for_service(self, service):
+        print 'Disabling proxy on {}...'.format(service)
+        for subcommand in ['-setwebproxystate', '-setsecurewebproxystate']:
+            self.run_networksetup_command(subcommand, service, 'Off')
 
-def wrap_mitmproxy():
-    service_name = primary_service_name()
+    def interface_name_to_service_name_map(self):
+        order = self.run_networksetup_command('-listnetworkserviceorder')
+        mapping = re.findall(r'\(\d+\)\s(.*)$\n\(.*Device: (.+)\)$', order, re.MULTILINE)
+        return dict([(b, a) for (a, b) in mapping])
 
-    if not proxy_enabled_for_service(service_name):
-        enable_proxy_for_service(service_name)
+    def run_command_with_input(self, command, input):
+        popen = subprocess.Popen(command, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+        (stdout, stderr) = popen.communicate(input)
+        return stdout
+    
+    def primary_interace_name(self):
+        scutil_script = 'get State:/Network/Global/IPv4\nd.show\n'
+        stdout = self.run_command_with_input('/usr/sbin/scutil', scutil_script)
+        interface, = re.findall(r'PrimaryInterface\s*:\s*(.+)', stdout)
+        return interface
 
-    subprocess.check_call(['mitmproxy', '--palette', 'light'])
+    def find_primary_service_name(self):
+        return self.interface_name_to_service_name_map()[self.primary_interace_name()]
 
-    if proxy_enabled_for_service(service_name):
-        disable_proxy_for_service(service_name)
+    def proxy_enabled_for_service(self, service):
+        return self.proxy_state_for_service(service)['Enabled'] == 'Yes'
 
-def main():
-    parser = argparse.ArgumentParser(description='Helper tool for OS X proxy configuration and mitmproxy')
-    parser.add_argument('--toggle', action='store_true', help='just toggle the proxy configuration')
-    args = parser.parse_args()
+    def toggle_proxy(self):
+        if self.proxy_enabled_for_service(self.primary_service_name):
+            self.disable_proxy_for_service(self.primary_service_name)
+        else:
+            self.enable_proxy_for_service(self.primary_service_name)
 
-    if args.toggle:
-        toggle_proxy()
-    else:
-        wrap_mitmproxy()
+    def wrap_mitmproxy(self):
+        if not self.proxy_enabled_for_service(self.primary_service_name):
+            self.enable_proxy_for_service(self.primary_service_name)
+
+        subprocess.check_call(['mitmproxy', '-p', str(self.port), '--palette', 'light'])
+
+        if self.proxy_enabled_for_service(self.primary_service_name):
+            self.disable_proxy_for_service(self.primary_service_name)
+
+    @classmethod
+    def main(cls):
+        parser = argparse.ArgumentParser(description='Helper tool for OS X proxy configuration and mitmproxy')
+        parser.add_argument('-t', '--toggle', action='store_true', help='just toggle the proxy configuration')
+        parser.add_argument('-p', '--port', type=int, help='override the default port of 8080', default=8080)
+        args = parser.parse_args()
+
+        wrapper = cls(port=args.port)
+        if args.toggle:
+            wrapper.toggle_proxy()
+        else:
+            wrapper.wrap_mitmproxy()
 
 if __name__ == '__main__':
-    main()
+    Wrapper.main()
 
