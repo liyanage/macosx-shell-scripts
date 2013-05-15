c992493f52808de1b3edfe8f976516dde100bcc7
Marc Liyanage <reg.github@entropy.ch>
2013-05-15 09:31:41 -0700

    Clean up setproxy script

--- setproxy.py
+++ setproxy.py
@@ -11,39 +11,54 @@
 import subprocess
 import re
 
+def run_networksetup_command(*arguments):
+    return subprocess.check_output(['sudo', 'networksetup'] + list(arguments))
 
-def proxy_state_for_interface(interface):
-    state = subprocess.check_output('networksetup -getwebproxy'.split() + [interface]).splitlines()
+def proxy_state_for_service(service):
+    state = run_networksetup_command('-getwebproxy', service).splitlines()
     return dict([re.findall(r'([^:]+): (.*)', line)[0] for line in state])
 
-def enable_proxy_for_interface(interface):
+def enable_proxy_for_service(service):
+    print 'Enabling proxy on {}...'.format(service)
     for subcommand in ['-setwebproxy', '-setsecurewebproxy']:
-        subprocess.check_output(['networksetup', subcommand, interface, '127.0.0.1', '8080'])
+        run_networksetup_command(subcommand, service, '127.0.0.1', '8080')
 
-def disable_proxy_for_interface(interface):
+def disable_proxy_for_service(service):
+    print 'Disabling proxy on {}...'.format(service)
     for subcommand in ['-setwebproxystate', '-setsecurewebproxystate']:
-        subprocess.check_output(['networksetup', subcommand, interface, 'Off'])
-    
-def first_interface():
-    scutil_script = 'get State:/Network/Global/IPv4\nd.show\n'
-    popen = subprocess.Popen('/usr/sbin/scutil', stdin=subprocess.PIPE, stdout=subprocess.PIPE)
-    (stdout, stderr) = popen.communicate(scutil_script)
-    interface, = re.findall(r'PrimaryInterface\s*:\s*(.+)', stdout)
+        run_networksetup_command(subcommand, service, 'Off')
 
-    order = subprocess.check_output('networksetup -listnetworkserviceorder'.split())
+def interface_name_to_service_name_map():    
+    order = run_networksetup_command('-listnetworkserviceorder')
     mapping = re.findall(r'\(\d+\)\s(.*)$\n\(.*Device: (.+)\)$', order, re.MULTILINE)
-    mapping = dict([(b, a) for (a, b) in mapping])
+    return dict([(b, a) for (a, b) in mapping])
+
+def run_command_with_input(command, input):
+    popen = subprocess.Popen(command, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+    (stdout, stderr) = popen.communicate(input)
+    return stdout
     
-    service_name = mapping[interface]
-    return service_name
+def primary_interace_name():
+    scutil_script = 'get State:/Network/Global/IPv4\nd.show\n'
+    stdout = run_command_with_input('/usr/sbin/scutil', scutil_script)
+    interface, = re.findall(r'PrimaryInterface\s*:\s*(.+)', stdout)
+    return interface
+
+def primary_service_name():
+    return interface_name_to_service_name_map()[primary_interace_name()]
+
+def proxy_enabled_for_service(service):
+    return proxy_state_for_service(service)['Enabled'] == 'Yes'
     
 
-first_interface = first_interface()
+def main():
+    service_name = primary_service_name()
 
-state = proxy_state_for_interface(first_interface)
-if state['Enabled'] == 'Yes':
-    print 'Disabling proxy on {}...'.format(first_interface)
-    disable_proxy_for_interface(first_interface)
-else:
-    print 'Enabling proxy on {}...'.format(first_interface)
-    enable_proxy_for_interface(first_interface)
+    if proxy_enabled_for_service(service_name):
+        disable_proxy_for_service(service_name)
+    else:
+        enable_proxy_for_service(service_name)
+
+if __name__ == '__main__':
+    main()
+
