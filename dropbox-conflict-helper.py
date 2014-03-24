c3d847d6f9180d987244f388f07d4fe8b667289b
Marc Liyanage <reg.github@entropy.ch>
2014-03-24 14:07:44 -0700

    Update dropbox conflict helper to suggest newest

--- dropbox-conflict-helper.py
+++ dropbox-conflict-helper.py
@@ -7,47 +7,123 @@
 # https://github.com/liyanage/macosx-shell-scripts
 #
 
-import argparse
+import re
 import os
 import sys
-import re
+import hashlib
+import datetime
+import argparse
 import subprocess
 
 
-class Duplicate(object):
+class DuplicateFile(object):
+
+    def __init__(self, path):
+        self.path = path
+        self.cached_hexdigest = None
+        self.cached_last_modified_timestamp = None
+    
+    def hexdigest(self):
+        if not self.cached_hexdigest:
+            hash = hashlib.new('md5')
+            with open(self.path) as f:
+                hash.update(f.read())
+            self.cached_hexdigest = hash.hexdigest()
+        return self.cached_hexdigest
+    
+    def last_modified_timestamp(self):
+        if not self.cached_last_modified_timestamp:
+            self.cached_last_modified_timestamp = datetime.datetime.fromtimestamp(os.stat(self.path).st_mtime)
+        return self.cached_last_modified_timestamp
+    
+    def is_symlink(self):
+        return os.path.islink(self.path)
+
+class DuplicateSet(object):
 
     def __init__(self, original_path):
-        self.original_path = original_path
-        self.duplicate_paths = []
+        self.original_file = DuplicateFile(original_path)
+        self.duplicate_files = []
 
     def add_duplicate_path(self, path):
-        self.duplicate_paths.append(path)
+        self.duplicate_files.append(DuplicateFile(path))
     
-    def summary(self):
-        summary = subprocess.check_output(['ls', '-l', self.original_path])
-        for duplicate_path in self.duplicate_paths:
-            summary += subprocess.check_output(['ls', '-l', duplicate_path])
+    def all_duplicates_are_identical(self):
+        if self.duplicates_contain_symlinks():
+            return False
+
+        hashes = set()
+        for file in self.all_files():
+            hashes.add(file.hexdigest())
+        return len(hashes) == 1
+
+    def duplicates_contain_symlinks(self):
+        for file in self.all_files():
+            if file.is_symlink():
+                return True
+        return False
+    
+    def all_files(self):
+        return [self.original_file] + self.duplicate_files
+    
+    def all_files_ordered_by_date(self):
+        return sorted(self.all_files(), cmp=lambda a, b: cmp(b.last_modified_timestamp(), a.last_modified_timestamp()))
+
+    def summary(self, keep_newest=False):
+        summary = ''
+        for duplicate_file in self.all_files_ordered_by_date():
+            summary += '# ' + subprocess.check_output(['ls', '-l', duplicate_file.path])
+
+        if self.all_duplicates_are_identical():
+            summary += '# All duplicates identical\n'
+        if self.duplicates_contain_symlinks():
+            summary += '# Duplicates contain symlinks\n'
         return summary
 
-    def worksheet(self):
-        worksheet = ''
-        for duplicate_path in self.duplicate_paths:
-           worksheet += 'diff -u "{}" "{}"\n'.format(self.original_path, duplicate_path)
-           worksheet += 'mv "{}" "{}"\n'.format(duplicate_path, self.original_path)
-           worksheet += 'rm "{}"\n'.format(duplicate_path, self.original_path)
-        return worksheet
+    def delete_all_duplicates_worksheet_content(self):
+        worksheet_content = ''
+        for duplicate_file in self.duplicate_files:
+            worksheet_content += 'rm "{}"\n'.format(duplicate_file.path, self.original_file.path)
+        return worksheet_content
+
+    def worksheet_content(self, keep_newest=False):
+        if self.all_duplicates_are_identical():
+            return self.delete_all_duplicates_worksheet_content()
+
+        worksheet_content = ''
+        if keep_newest:
+            all_files = self.all_files_ordered_by_date()
+            newest_file = all_files[0]
+            if self.original_file == newest_file:
+                return self.delete_all_duplicates_worksheet_content()
+            
+            original_path = self.original_file.path
+            for file in all_files[1:]:
+                worksheet_content += '# diff -u "{}" "{}"\n'.format(newest_file.path, file.path)
+                if file.path != original_path:
+                    worksheet_content += 'rm "{}"\n'.format(file.path)
+            worksheet_content += 'mv "{}" "{}"\n'.format(newest_file.path, original_path)
+            return worksheet_content
+
+        for duplicate_file in self.duplicate_files:
+            if not self.all_duplicates_are_identical():
+                worksheet_content += 'diff -u "{}" "{}"\n'.format(self.original_file.path, duplicate_file.path)
+                worksheet_content += 'mv "{}" "{}"\n'.format(duplicate_file.path, self.original_file.path)
+            worksheet_content += 'rm "{}"\n'.format(duplicate_file.path)
+        return worksheet_content
     
     def __str__(self):
-        return '<Duplicate {}>'.format(self.original_path)
+        return '<Duplicate {}>'.format(self.original_file.path)
 
 
 class Tool(object):
 
-    def __init__(self, dropbox_path):
-        self.dropbox_path = os.path.expanduser(dropbox_path)
+    def __init__(self, args):
+        self.args = args
+        self.dropbox_path = os.path.expanduser(args.dropbox_path)
         assert os.path.exists(self.dropbox_path), 'Invalid Dropbox path {}'.format(dropbox_path)
-        self.duplicates = []
-        self.duplicate_map = {}
+        self.duplicate_sets = []
+        self.duplicate_set_map = {}
         self.running_from_worksheet = 'BBEDIT_CLIENT_INTERACTIVE' in os.environ
     
     def run(self):
@@ -55,6 +131,8 @@ class Tool(object):
         self.process_duplicates()
     
     def gather_duplicates(self):
+        limit = 0
+        count = 0
         for root, dirs, files in os.walk(self.dropbox_path):
             if '.dropbox.cache' in dirs:
                 del(dirs[dirs.index('.dropbox.cache')])
@@ -63,48 +141,53 @@ class Tool(object):
                 if not match:
                     continue
 
+                count += 1
+                if limit and count > limit:
+                    return
+
                 original_name = match.group(1)
                 extension = match.group(2)
                 if extension:
                     original_name += extension
 
                 full_path = os.path.join(root, original_name)
-                duplicate = self.duplicate_for_original_path(full_path)
-                duplicate.add_duplicate_path(os.path.join(root, file))
+                duplicate_set = self.duplicate_set_for_original_path(full_path)
+                duplicate_set.add_duplicate_path(os.path.join(root, file))
 
     def process_duplicates(self):
-        if not self.duplicates:
+        if not self.duplicate_sets:
             print 'No conflicted files found'
             return
             
         if not self.running_from_worksheet:
             bbedit = subprocess.Popen(['bbedit', '-s'], stdin=subprocess.PIPE)
             
-        for duplicate in self.duplicates:
-            text = '# {}\n'.format(duplicate)
-            text += duplicate.summary()
-            text += duplicate.worksheet()
+        for duplicate_set in self.duplicate_sets:
+            text = '# {}\n'.format(duplicate_set)
+            text += duplicate_set.summary(keep_newest=self.args.keep_newest)
+            text += duplicate_set.worksheet_content(keep_newest=self.args.keep_newest)
             text += '\n'
             if self.running_from_worksheet:
                 sys.stdout.write(text)
             else:
                 bbedit.stdin.write(text)
 
-    def duplicate_for_original_path(self, path):
-        duplicate = self.duplicate_map.get(path, None)
-        if not duplicate:
-            duplicate = Duplicate(path)
-            self.duplicates.append(duplicate)
-            self.duplicate_map[path] = duplicate
-        return duplicate
+    def duplicate_set_for_original_path(self, path):
+        duplicate_set = self.duplicate_set_map.get(path, None)
+        if not duplicate_set:
+            duplicate_set = DuplicateSet(path)
+            self.duplicate_sets.append(duplicate_set)
+            self.duplicate_set_map[path] = duplicate_set
+        return duplicate_set
 
     @classmethod
     def main(cls):
         parser = argparse.ArgumentParser(description='Helper to process Dropbox conflict duplicates')
         parser.add_argument('dropbox_path', help='Path to Drobox folder')
+        parser.add_argument('-n', '--keep-newest', action='store_true', default=True, help='Default to keeping the newest duplicate of a file')
 
         args = parser.parse_args()
-        cls(dropbox_path=args.dropbox_path).run()
+        cls(args).run()
 
 
 if __name__ == '__main__':
