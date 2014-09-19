3672cf714d58b0de1802fb05d4d31d536c41a706
Marc Liyanage <reg.github@entropy.ch>
2014-09-18 17:37:09 -0700

    Cleanup, remove some old unused scripts and move other less used ones to an "archived" subdirectory

--- add-leading-zero.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-#
-# add leading zeros to single-digit photo files.
-
-for i in ?\ *.jpg; do mv "$i" "0$i"; done
-for i in ??\ *.jpg; do mv "$i" "0$i"; done
-
--- unixtime.rb
+++
@@ -1,9 +0,0 @@
-#!/usr/local/bin/macruby
-
-if ARGV.empty?
-  puts "Usage: unixtime.rb <unix-time-value>"
-  exit
-end
-
-puts NSDate.dateWithTimeIntervalSince1970(ARGV[0]).description
-
