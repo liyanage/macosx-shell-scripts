8f2b03b6720f633377c21048b63b1c318df46179
Marc Liyanage <reg.github@entropy.ch>
2017-10-10 07:22:56 -0700

    script updates

--- kill-xcode-cache.sh
+++ kill-xcode-cache.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 find $(dirname $TMPDIR)/C -type d -name Shared* -exec rm -r {} \; -prune
+find ~/Library/Developer/Xcode/DerivedData -type d -name PrecompiledHeaders -exec rm -rf {} \; -prune
 
